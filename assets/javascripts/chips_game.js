aaa911e258650e85f21fd21b5ae0fe3a080de114
Elizabeth Sparks <erhsparks@gmail.com>
2016-11-17 01:14:10 -0800

    refactor out dead code, Chip moves with consideraly less dead weight now!

--- assets/javascripts/chips_game.js
+++ assets/javascripts/chips_game.js
@@ -11,32 +11,13 @@ class ChipsChallenge {
   }
 
   listenforArrowKeys() {
-    this.arrowKeysPressed = {
-      ArrowUp: false,
-      ArrowDown: false,
-      ArrowLeft: false,
-      ArrowRight: false
-    };
-
     d3.select('body')
-      .on('keydown', () => this.handleKeypress(d3.event.key, 'down'))
-      .on('keyup', () => this.handleKeypress(d3.event.key, 'up'));
+      .on('keydown', () => this.handleKeypress(d3.event.key));
   }
 
   handleKeypress (key, upOrDown) {
-    if (Object.keys(this.arrowKeysPressed).includes(key)) {
-      switch (upOrDown) {
-        case 'down':
-          this.arrowKeysPressed[key] = true;
-          this.moveChip(key);
-          break;
-        case 'up':
-          this.arrowKeysPressed[key] = false;
-          break;
-        default:
-          console.log('something else??');
-      }
-    }
+    let arrowKeys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
+    if (arrowKeys.includes(key)) this.moveChip(key);
   }
 
   moveChip(direction) {
@@ -47,22 +28,18 @@ class ChipsChallenge {
 
     switch (direction) {
       case 'ArrowUp':
-        console.log("Uptown girl, she's been living in her uptown world");
         y -= dXY;
         chip.attr('y', `${y}`);
         break;
       case 'ArrowDown':
-        console.log("When you're alone and life is making you lonely, you can always go... downtown");
         y += dXY;
         chip.attr('y', `${y}`);
         break;
       case 'ArrowLeft':
-        console.log("Slam it to the left if you're havin' a good time");
         x -= dXY;
         chip.attr('x', `${x}`);
         break;
       case 'ArrowRight':
-        console.log("Shake it to the right if you know that you feel fine");
         x += dXY;
         chip.attr('x', `${x}`);
         break;
