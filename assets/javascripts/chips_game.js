9cc3a6d720bdb2c1c23a76a49c00b4aafe0cc6da
Elizabeth Sparks <erhsparks@gmail.com>
2016-11-16 23:39:32 -0800

    preliminary arrow key bindings

--- assets/javascripts/chips_game.js
+++ assets/javascripts/chips_game.js
@@ -1,8 +1,59 @@
+import * as d3 from "d3";
 import LevelOneMap from './map';
 
 class ChipsChallenge {
   constructor() {
-    this.board = new LevelOneMap();
+    this.gameMap = new LevelOneMap();
+
+    this.listenforKeyPresses();
+    this.handleKeypress = this.handleKeypress.bind(this);
+    this.moveChip = this.moveChip.bind(this);
+  }
+
+  listenforKeyPresses() {
+    var arrowKeysPressed = {
+      ArrowUp: false,
+      ArrowDown: false,
+      ArrowLeft: false,
+      ArrowRight: false
+    };
+
+    d3.select('body')
+      .on('keydown', () => {
+        let key = d3.event.key;
+        if (Object.keys(arrowKeysPressed).includes(key)) {
+          arrowKeysPressed[key] = true;
+          this.moveChip();
+        }
+      })
+      .on('keyup', () => {
+        let key = d3.event.key;
+        if (Object.keys(arrowKeysPressed).includes(key)) {
+          arrowKeysPressed[d3.event.key] = false;
+          console.log('key released!');
+        }
+      });
+  }
+
+  // handleKeypress(key, type) {
+  //   debugger
+  //   console.log(key);
+  //
+  //   var keyPressed = {};
+  //   switch (type) {
+  //     case 'down':
+  //       keyPressed[keyId] = true;
+  //       break;
+  //     case 'up':
+  //       keyPressed[keyId] = false;
+  //       break;
+  //     default:
+  //       console.log('not a keypress??');
+  //   }
+  // }
+
+  moveChip() {
+    console.log("hooray, we're getting somewhere!");
   }
 }
 
