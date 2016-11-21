d183b92bdc8cfa8a18f107c9e188f57102f0b3ac
Elizabeth Sparks <erhsparks@gmail.com>
2016-11-21 00:39:41 -0800

    move .js files to lib/, rm assets/javascripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   context: __dirname,
-  entry: './assets/javascripts/main.js',
+  entry: './lib/main.js',
   output: {
-    path: './assets/javascripts',
+    path: './',
     filename: 'bundle.js',
     devtoolModuleFilenameTemplate: '[resourcePath]',
     devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
